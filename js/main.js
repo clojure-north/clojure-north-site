dc8d6d37950f231e51a229743b98eb024179a7f2
Carmen La <carmen.wla@gmail.com>
2018-11-26 18:54:17 -0500

    cleanup

--- js/main.js
+++ js/main.js
@@ -34,14 +34,6 @@ jQuery(document).ready(function( $ ) {
   // Initiate the wowjs animation library
   new WOW().init();
 
-  // Initialize Venobox
-  $('.venobox').venobox({
-    bgcolor: '',
-    overlayColor: 'rgba(6, 12, 34, 0.85)',
-    closeBackground: '',
-    closeColor: '#fff'
-  });
-
   // Initiate superfish on nav menu
   $('.nav-menu').superfish({
     animation: {
@@ -123,24 +115,6 @@ jQuery(document).ready(function( $ ) {
       }
     }
   });
-
-  // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center:true,
-    responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
-    }
-  });
-
-  // Buy tickets select the ticket type on click
-  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var ticketType = button.data('ticket-type');
-    var modal = $(this);
-    modal.find('#ticket-type').val(ticketType);
-  })
 
 // custom code
 
